204531ee9e9bd9060af230c0d317f137574ede9e
Komoliddin <tegirmon@gmail.com>
2015-05-02 16:23:54 -0400

    pic update

--- whynotMain/urls.py
+++ whynotMain/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^efforts/', views.efforts, name='efforts'),
+    url(r'^rankings/', views.rankings, name='rankings'),
 ]
