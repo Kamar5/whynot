6ea66def2321eb037f3a88581af2aec9eba86299
Komoliddin <tegirmon@gmail.com>
2015-05-02 14:44:58 -0400

    Templates moved to whynotMain

--- whynot/urls.py
+++ whynot/urls.py
@@ -1,10 +1,5 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import include, url
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'whynot.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^$', include('whynotMain.urls')),
+]
