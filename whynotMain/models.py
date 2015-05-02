702fdc674e28eb7839df57a1eba411d5a63d8c4f
Jonathan Amenechi <jonathanamenechi@yahoo.com>
2015-05-02 12:51:10 -0400

    Updated models, settings and removed migrations folder

--- whynotMain/models.py
+++ whynotMain/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Login(models.Model):
+    name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    #Not sure whether to store the actual picture or a link to the picture that is stored on the filesystem
+    #For now we're going to use filepath
+    profile_pic = models.CharField(max_length=40)
+
+class Pic_Actions(models.Model):
+    username = models.CharField(max_length=30) #Uploader
+    date = models.CharField(max_length=40) 
+    pic_before = models.CharField(max_length=30) #Before Pic Filepath
+    pic_after = models.CharField(max_length=30)  #After Pic filepath
+    score = models.PositiveSmallIntegerField(default=1) #Picture Score
+    
+   
+
+
+    
+
