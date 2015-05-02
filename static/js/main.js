2d4c07082be078155dee5db3f571bbb10fc20bb3
Komoliddin <tegirmon@gmail.com>
2015-05-02 13:56:20 -0400

    Added Templates & Static files
    adding templates, css, js and image files

---
+++ static/js/main.js
@@ -0,0 +1,31 @@
+$(function() {
+    console.log("starting...");
+    var thumbsoup = "fa-thumbs-o-up";
+    var thumbsup = "fa-thumbs-up";
+    var good = "Good Job!";
+    var awesome = "Awesome Effort!";
+    var unbelivable = "I am going to cry!";
+    $(".vote-button").hover(
+        function() {
+    		$(this).addClass(thumbsup).removeClass(thumbsoup);
+    		$(this).prev().addClass(thumbsup).removeClass(thumbsoup);
+    		$(this).prev().prev().addClass(thumbsup).removeClass(thumbsoup);
+    		var index = $(this).index();
+    		console.log(index);
+
+    		if(index == 0){
+    			$(this).siblings(".vote-message").html(good);
+    		} else if(index == 1){
+$(this).siblings(".vote-message").html(awesome);
+    		} else if(index == 2){
+    			$(this).siblings(".vote-message").html(unbelivable);
+    		}
+        },
+        function() {
+    		$(this).removeClass(thumbsup).addClass(thumbsoup);
+    		$(this).prev().removeClass(thumbsup).addClass(thumbsoup);
+    		$(this).prev().prev().removeClass(thumbsup).addClass(thumbsoup);
+    		$(this).siblings(".vote-message").html("");
+        }
+    );
+});
